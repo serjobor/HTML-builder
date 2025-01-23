6e8d3dde5f22a31333cd2b79ab67627a8f52cdae
serjobor <borisov_sergey2002@mail.ru>
2025-01-23 17:33:32 +0400

    fix: fix bag old files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,33 +7,40 @@ const pathToFolderFilesCopy = path.join(__dirname, 'files-copy');
 const testArr = [];
 
 const copyDir = () => {
-  fs.readdir(pathToFolderFiles, (err, files) => {
+
+  fs.rm(pathToFolderFilesCopy, { recursive: true, force: true }, (err) => {
     if (err) {
-      console.log(err);
+      return console.log(err);
     }
-    else {
-      files.forEach(file => {
-        testArr.push(file);
-        // console.log(file);      
-      });
 
-      fs.mkdir(pathToFolderFilesCopy, { recursive: true }, (err) => {
-        if (err) {
-          return console.log(err);
-        }
-
-        for (const key in testArr) {
-          fs.copyFile(path.join(pathToFolderFiles, `${testArr[key]}`), 
-                path.join(pathToFolderFilesCopy, `${testArr[key]}`), 
-                err => {
-            if (err) {
-              console.log(err);
-            }
-          });
-        }
-        console.log('Directory created');
-      });
-    };
+    fs.readdir(pathToFolderFiles, (err, files) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        files.forEach(file => {
+          testArr.push(file);
+          // console.log(file);      
+        });
+  
+        fs.mkdir(pathToFolderFilesCopy, { recursive: true }, (err) => {
+          if (err) {
+            return console.log(err);
+          }
+  
+          for (const key in testArr) {
+            fs.copyFile(path.join(pathToFolderFiles, `${testArr[key]}`), 
+                  path.join(pathToFolderFilesCopy, `${testArr[key]}`), 
+                  err => {
+              if (err) {
+                console.log(err);
+              }
+            });
+          }
+          console.log('Directory created');
+        });
+      };
+    });
   });
 };
 
